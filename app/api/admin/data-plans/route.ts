816a8a13f5eb9cf7570935b2feb40f26cb839f00
Muhammad Bello Ibrahim <ug20sccs1026@gsu.edu.ng>
2025-10-10 02:05:10 +0100

    Set dynamic route handling for data plans

--- app/api/admin/data-plans/route.ts
+++ app/api/admin/data-plans/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from "next/server";
 import DataPlan from "@/lib/models/DataPlan";
 import { Database } from "@/lib/database";
