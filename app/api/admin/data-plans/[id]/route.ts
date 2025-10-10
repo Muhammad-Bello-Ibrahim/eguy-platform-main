8d3da6f0db25f743891f9c89142b7e8f36ed51e3
Muhammad Bello Ibrahim <ug20sccs1026@gsu.edu.ng>
2025-10-10 02:20:26 +0100

    Update route.ts

--- app/api/admin/data-plans/[id]/route.ts
+++ app/api/admin/data-plans/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import DataPlan from "@/lib/models/DataPlan";
 import { Database } from "@/lib/database";
-
+export const dynamic = "force-dynamic";
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   await Database.getDb();
   const body = await req.json();
